7500f6d0529b0c413115fcc24cbfd33f02d370c0
yoshiki625 <yoshiki_hoshikawa@niandc.co.jp>
2024-11-05 15:40:29 +0530

    Comparison & Logical operetors

---
+++ Day_2.0_round.py
@@ -0,0 +1,11 @@
+# Task 2.1: With f-string
+# Fahrenheit to Celcius
+# Please provide your Fahrenheit: 98.6
+# The 98.6°F is 37°C
+# °C = (°F - 32) × 5/9
+
+  
+fahrenheit = float(input("Please provide your Fahrenheit: "))
+celcius = round((fahrenheit - 32) * 5/9,2)
+print(f"The  {fahrenheit}°F is {celcius}°C")
+
---
+++ Day_2.1_operetors_2.py
@@ -0,0 +1,18 @@
+# Equal
+print(5 == 5)
+# Not Equal
+print(5 != 3)
+# Greater Than
+print(5 > 3)
+# Less Than
+print(3 < 5)
+# Greater Than or Equal to
+print(5 >= 5)
+# Less Than or Equal to
+print(3 <= 5)
+
+# Logical operators
+
+# 1. and -> Both are True
+# 2. or -> Any one True
+# 3. not -> Opposite
